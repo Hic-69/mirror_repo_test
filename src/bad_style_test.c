d98622e7f7352192feded75c600c247d70546b9f
Hic-69 <franck.avoce@epitech.eu>
2026-08-14 16:37:49 +0100

    reteste

---
+++ src/bad_style_test.c
@@ -0,0 +1,6 @@
+int badly_named_function_that_is_way_too_long_for_the_epitech_naming_convention(int a,int b,int c,int d,int e) {
+    if (a == 1) {
+            return a+b+c+d+e;
+    }
+  return 0;
+}
